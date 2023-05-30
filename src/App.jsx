e03d61a8ef37cfcbfaeebf4851a01209aa741b10
Tony Baglien <tbaglien@crescendocollective.com>
2023-05-30 13:29:21 -0500

    added in input component and stylings, as well as basic logic.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,37 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { useContext } from "react";
+
+import Input from "./components/Input";
+import { Context } from "./store";
+
 function App() {
+    const [phrase] = useContext(Context);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`the phrase you entered was : ${phrase}`);
+    };
+
     return (
-        <div className="w-screen h-screen bg-cyan-500 flex items-center justify-center">
-            <h1 className="text-red-50">Hello world</h1>
-        </div>
+        <form
+            onSubmit={handleSubmit}
+            className="w-screen h-screen bg-blue-powder flex items-center justify-center flex-col"
+        >
+            <Input
+                labelClass="block mb-2 text-3xl text-gray-800"
+                phrase="Enter your phrase"
+                inputClass="w-full shadow appearance-none border-none rounded py-2 px-3 mb-4 text-2xl text-blue-gray uppercase focus:outline-none"
+                placeholder=""
+            />
+            <button
+                className="block border-none shadow-lg py-2 px-6 text-3xl rounded-lg text-white bg-blue-gray"
+                type="submit"
+            >
+                Generate
+            </button>
+        </form>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "blue-light": "#C5D1EB",
+                "blue-powder": "#92AFD7",
+                "blue-gray": "#5A7684",
+                "green-light": "#395B50",
+                "green-dark": "#1F2F16",
+            },
+        },
     },
     plugins: [],
 };
